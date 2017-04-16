8292790a79150d81aa6bbf07558ac041537ffef4
jwaltzjr <jwaltzjr@hotmail.com>
2017-04-16 00:35:17 -0500

    Update production settings

--- ifitscool/ifitscool/settings.py
+++ ifitscool/ifitscool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('IFITSCOOLKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['162.243.202.15']
 
 
 # Application definition
